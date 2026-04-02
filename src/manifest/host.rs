57d18d593081c567de4dc3a6ac09f630d43c83cc
Alexander Milchinskiy <milchinskiy@gmail.com>
2026-04-02 14:38:40 +0300

    update manifest host definition

--- src/manifest/host.rs
+++ src/manifest/host.rs
@@ -12,6 +12,18 @@ pub enum SshHostKeyPolicy {
     Strict { path: Option<PathBuf> },
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, serde::Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum SshAuth {
+    Agent,
+    KeyFile {
+        private_key: PathBuf,
+        public_key: Option<PathBuf>,
+        passphrase: Option<String>,
+    },
+    Password,
+}
+
 #[derive(Clone, serde::Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub struct HostSshConnection {
@@ -19,6 +31,7 @@ pub struct HostSshConnection {
     pub host: String,
     pub port: Option<u16>,
     pub host_key_policy: Option<SshHostKeyPolicy>,
+    pub auth: SshAuth,
 
     #[serde(default, with = "serde_ext_duration::opt::human")]
     pub connect_timeout: Option<Duration>,
@@ -32,7 +45,7 @@ pub struct HostSshConnection {
 #[serde(rename_all = "snake_case")]
 pub enum HostKind {
     Local,
-    Ssh(HostSshConnection),
+    Ssh(Box<HostSshConnection>),
 }
 
 #[derive(Clone, serde::Deserialize)]
@@ -41,4 +54,28 @@ pub struct Host {
     pub id: HostId,
     pub tags: Option<Vec<HostTag>>,
     pub kind: HostKind,
+}
+
+impl Host {
+    pub fn matches(&self, selector: &HostSelector) -> bool {
+        match selector {
+            HostSelector::Id(id) => &self.id == id,
+            HostSelector::Tag(tag) => self.tags.as_ref().is_some_and(|tags| tags.contains(tag)),
+            HostSelector::List(list) => list.iter().any(|s| self.matches(s)),
+        }
+    }
+}
+
+#[derive(Clone, serde::Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum HostSelector {
+    Id(HostId),
+    Tag(HostTag),
+    List(Vec<Self>),
+}
+
+impl HostSelector {
+    pub fn matches(&self, host: &Host) -> bool {
+        host.matches(self)
+    }
 }
