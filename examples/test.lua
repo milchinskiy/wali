80f8792a7c9e0fcd502a18cd41c5904fae939b68
Alexander Milchinskiy <milchinskiy@gmail.com>
2026-04-07 02:05:41 +0300

    extend example config

--- examples/test.lua
+++ examples/test.lua
@@ -6,13 +6,38 @@ return {
 			tags = { "local" },
 			vars = { user = "test-user" },
 		},
-		-- { id = "local", transport = "local" },
+		{
+			id = "ssh-test",
+			transport = {
+				ssh = {
+					host = "1.2.3.4",
+					user = "test-user",
+				},
+			},
+			tags = { "remote", "ssh" },
+			vars = { user = "remote-user" },
+		},
 	},
 
-    modules = {
-        { path = "../docs" },
-        { path = "../docs" },
-    },
+	modules = {
+		{ path = "../docs" },
+	},
 
-	tasks = {},
+	tasks = {
+		{
+			id = "test task #1",
+			tags = { "task-tag-1" },
+			when = {
+				all = {
+					{ hostname = "test-hostname" },
+					{ os = "linux" },
+					{ arch = "x86_64" },
+					{ env_set = "DISPLAY" },
+				},
+			},
+			host = { ["not"] = { tag = "remote" } },
+			module = { builtin = "wali.test.module" },
+			args = { path1 = "test", path2 = "../examples" },
+		},
+	},
 }
