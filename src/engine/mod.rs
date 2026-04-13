f0670b9787bdb2dd9433bc0d17d97f6df0ae71aa
Alexander Milchinskiy <milchinskiy@gmail.com>
2026-04-13 19:04:57 +0300

    implement worker

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,5 +1,8 @@
 use std::collections::BTreeMap;
 
+use crate::executor::Executor;
+use crate::plan::HostPlan;
+
 pub struct Engine {
     workers: Vec<Worker>,
 }
@@ -7,6 +10,32 @@ pub struct Engine {
 pub struct Worker {
     lua: crate::lua::LuaRuntime,
     modules: BTreeMap<String, crate::lua::module::Module>,
-    host_plan: crate::plan::HostPlan,
-    executor: Box<dyn crate::executor::Executor>,
+    host_plan: HostPlan,
+    executor: Box<dyn Executor>,
+}
+
+impl Worker {
+    pub fn new(host_plan: HostPlan, executor: impl Executor + 'static) -> crate::Result<Self> {
+        let lua = crate::lua::LuaRuntime::with_modules_flow()?;
+        for path in &host_plan.modules_paths {
+            lua.add_include_path(path)?;
+        }
+
+        let modules = host_plan
+            .tasks
+            .iter()
+            .map(|task| -> crate::Result<_> {
+                let module_name = task.module.clone();
+                let module = lua.module_load_by_name(&module_name)?;
+                Ok((module_name, module))
+            })
+            .collect::<crate::Result<BTreeMap<_, _>>>()?;
+
+        Ok(Self {
+            lua,
+            modules,
+            host_plan,
+            executor: Box::new(executor),
+        })
+    }
 }
